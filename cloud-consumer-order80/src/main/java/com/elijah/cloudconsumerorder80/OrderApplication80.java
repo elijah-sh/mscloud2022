f7c52dd574f80797dd07fab1162684ecbf480b6d
沈帅虎 <shuaihu.shen@hand-china.com>
2022-09-12 17:53:35 +0800

    [ADD] 添加微服务项目基本模版  父、订单、支付模块 文档

---
+++ cloud-consumer-order80/src/main/java/com/elijah/cloudconsumerorder80/OrderApplication80.java
@@ -0,0 +1,20 @@
+package com.elijah.cloudconsumerorder80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 对于服务调用这来说 只需要调用 即controller即可
+ *
+ * @author elijah
+ * @version 1.0
+ * @date 2022/9/3 11:21
+ */
+@SpringBootApplication
+public class OrderApplication80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication80.class, args);
+    }
+
+}
